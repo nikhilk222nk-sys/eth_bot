502958104fd259ca7fce77a107278722576839bb
nikhilk222nk-sys <nikhilatltr@gmail.com>
2026-05-25 22:38:48 +0530

    fixed telegram env variables

--- telegram_alerts.py
+++ telegram_alerts.py
@@ -1,11 +1,13 @@
-from dotenv import load_dotenv
 import os
 import requests
 
+from dotenv import load_dotenv
+
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8936512534:AAGxFC9AIxbJDXPUBYZ2gsUq07MEy7wM3ZU")
-CHAT_ID = os.getenv("7366145742")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 
 class TelegramAlerts:
@@ -14,28 +16,25 @@ class TelegramAlerts:
     def send_message(message):
 
         if not BOT_TOKEN or not CHAT_ID:
+
             print("Telegram environment variables missing")
             return
 
         url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
         payload = {
-            "chat_id": CHAT_ID,
-            "text": message
+            'chat_id': CHAT_ID,
+            'text': message
         }
 
         try:
 
-            response = requests.post(
+            requests.post(
                 url,
-                data=payload,
-                timeout=10
+                data=payload
             )
 
-            if response.status_code == 200:
-                print("Telegram Alert Sent")
-            else:
-                print(f"Telegram Failed: {response.text}")
+            print("Telegram Alert Sent")
 
         except Exception as e:
 
